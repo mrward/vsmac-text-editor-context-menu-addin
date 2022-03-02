2308c330d891625c38b204d51315b84ca3a04c82
Matt Ward <matt.ward@microsoft.com>
2022-03-02 22:50:35 +0000

    Add sample addin

---
+++ TextEditorContextMenuAddin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Addins;
+
+[assembly: Addin (
+	"TextEditorContextMenuAddin",
+	Namespace = "MonoDevelop",
+	Version = "0.1",
+	Category = "IDE extensions")]
+
+[assembly: AddinName ("TextEditorContextMenuAddin")]
+[assembly: AddinDescription ("")]
+
+[assembly: AddinDependency ("::MonoDevelop.Core", "8.0")]
+[assembly: AddinDependency ("::MonoDevelop.Ide", "8.0")]
+[assembly: AddinDependency ("::MonoDevelop.TextEditor", "8.0")]
